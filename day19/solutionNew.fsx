253f17ba7be8877b196e1c645fbda3be45ca67e2
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2023-04-21 14:18:16 +0100

    Lunch well spent, working d19p2

--- day19/solutionNew.fsx
+++ day19/solutionNew.fsx
@@ -1,6 +1,7 @@
 open System.IO
 open System.Text.RegularExpressions
 open System.Collections.Generic
+open System.Threading
 
 let blueprintPattern = Regex @"^Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.$"
 
@@ -14,8 +15,11 @@ type Minerals = {
     ore:int;
     clay:int;
     obsidian:int;
-    geode:int
+    geode:int;
 }
+
+let toString (minerals:Minerals) =
+    $"{minerals.ore} ore, {minerals.clay} clay, {minerals.obsidian} obsidian, {minerals.geode} geode"
 
 type Blueprint = {
     id:int;
@@ -54,37 +58,45 @@ let produce (robots:Minerals) (materials:Minerals) (turns:int) =
     {
         ore=(materials.ore + (robots.ore * turns));
         clay=(materials.clay + (robots.clay * turns));
-        obsidian=(materials.obsidian + robots.obsidian);
-        geode=(materials.geode + robots.geode)
+        obsidian=(materials.obsidian + (robots.obsidian * turns));
+        geode=(materials.geode + (robots.geode * turns))
     }
 
-let greaterThan (a:Minerals) (b:Minerals) =
-    (a.ore >= b.ore) && (a.clay >= b.clay) && (a.obsidian >= b.obsidian) && (a.geode >= b.geode)
+let divideAndRoundUp (a:int) (b:int) =
+    match a >= b with
+    | true ->
+        match a % b with
+        | 0 -> a / b
+        | _ -> (a / b) + 1
+    | false -> 0
+
+let calculateTime (cost:int) (materials:int) (robots:int) =
+    match materials with
+    | m when m >= cost -> 0
+    | _ ->
+        let toMake = cost - materials
+        let turns = toMake / robots
+        if toMake % robots = 0 then turns else turns + 1
 
 let timeToBuild (blueprint:Blueprint) (robots:Minerals) (materials:Minerals) (target:Mineral) =
     match target with
-    | Ore -> (blueprint.oreCost.ore - materials.ore) / robots.ore
-    | Clay -> (blueprint.clayCost.ore - materials.ore) / robots.ore
-    | Obsidian -> [| (blueprint.obsidianCost.ore - materials.ore) / robots.ore; (blueprint.obsidianCost.clay - materials.clay) / robots.clay |] |> Array.max
-    | Geode -> [| (blueprint.geodeCost.ore - materials.ore) / robots.ore; (blueprint.geodeCost.obsidian - materials.obsidian) / robots.obsidian |] |> Array.max
+    | Ore -> calculateTime blueprint.oreCost.ore materials.ore robots.ore
+    | Clay -> calculateTime blueprint.clayCost.ore materials.ore robots.ore
+    | Obsidian -> [| calculateTime blueprint.obsidianCost.ore materials.ore robots.ore; calculateTime blueprint.obsidianCost.clay materials.clay robots.clay |] |> Array.max
+    | Geode -> [| calculateTime blueprint.geodeCost.ore materials.ore robots.ore; calculateTime blueprint.geodeCost.obsidian materials.obsidian robots.obsidian |] |> Array.max
 
-let getOptions (blueprint:Blueprint) (robots:Minerals) =
-    // let canAffordOre = greaterThan materials blueprint.oreCost
-    // let canAffordClay = greaterThan materials blueprint.clayCost
-    // let canAffordObsidian = greaterThan materials blueprint.obsidianCost
-    // let canAffordGeode = greaterThan materials blueprint.geodeCost
-
+let getOptions (blueprint:Blueprint) (robots:Minerals) (materials:Minerals) (turns:int) =
     let maxOreCost = [| blueprint.oreCost.ore; blueprint.clayCost.ore; blueprint.obsidianCost.ore; blueprint.geodeCost.ore |] |> Array.max
+    let enoughClay = materials.clay + (robots.clay * turns) > (blueprint.obsidianCost.clay * turns)
+    let enoughObsidian = materials.obsidian + (robots.obsidian * turns) > (blueprint.geodeCost.obsidian * turns)
 
     seq {
-        // if (not canAffordOre || not canAffordClay || not canAffordObsidian || not canAffordGeode) then yield None
-        if robots.ore < maxOreCost then yield Some Ore
-        if robots.clay < blueprint.obsidianCost.clay then yield Some Clay
-        if robots.clay > 0 && robots.obsidian < blueprint.geodeCost.obsidian then yield Some Obsidian
-        if robots.obsidian > 0 then yield Some Geode
+        if robots.ore < maxOreCost then yield Ore
+        if not enoughClay then yield Clay
+        if robots.clay > 0 && not enoughObsidian then yield Obsidian
+        if robots.obsidian > 0 then yield Geode
     }
     |> Seq.toArray
-    |> Array.choose id
 
 let subtract (a:Minerals) (b:Minerals) =
     {
@@ -94,45 +106,82 @@ let subtract (a:Minerals) (b:Minerals) =
         geode=(a.geode - b.geode)
     }
 
+let getCost (blueprint:Blueprint) (robot:Mineral) =
+    match robot with
+    | Ore -> blueprint.oreCost
+    | Clay -> blueprint.clayCost
+    | Obsidian -> blueprint.obsidianCost
+    | Geode -> blueprint.geodeCost
+
+let buildRobot (robots:Minerals) (robot:Mineral) =
+    match robot with
+    | Ore -> {robots with ore=(robots.ore + 1)}
+    | Clay -> {robots with clay=(robots.clay + 1)}
+    | Obsidian -> {robots with obsidian=(robots.obsidian + 1)}
+    | Geode -> {robots with geode=(robots.geode + 1)}
+
+let pprint (robots:Minerals) (materials:Minerals) (turns:int) =
+    let turn = 24 - turns
+    printfn $"robots: {toString robots};  materials: {toString materials};  turn {turn}"
+
 let run (turns:int) (blueprint:Blueprint) =
-    let cache = Dictionary<Minerals*Minerals*int, int>()
+    let mutable count = 0L
+    let rec recurse (robots:Minerals) (materials:Minerals) (turns:int) (log:bool) =
+        // printfn "%A, %A, %i" robots materials turns
+        // Thread.Sleep 500
+        // let logging =
+        //     // log || (
+        //     // turns = 13 &&
+        //     // robots.ore = 1 &&
+        //     // robots.clay = 3 &&
+        //     // robots.obsidian = 1 &&
+        //     // robots.geode = 0 &&
+        //     // materials.ore = 2 &&
+        //     // materials.clay = 4 &&
+        //     // materials.obsidian = 0 &&
+        //     // materials.geode = 0)
+        //     count < 1000L
 
-    let rec recurse (robots:Minerals) (materials:Minerals) (turns:int) =
-        let entry = (robots,materials,turns)
-        match cache.ContainsKey entry with
-        | true ->
-            cache[entry]
-        | false ->
-            let result = 
-                match turns with
-                | 0 -> materials.geode
-                | _ ->
-                    // let nextTurn = turns - 1
+        // if logging then
+        //     pprint robots materials turns
 
-                    getOptions blueprint robots
-                    |> Array.map (fun robot ->
-                        match robot with
-                        // | None -> recurse robots (produce robots materials) nextTurn
-                        | Ore ->
-                            let waitTurns = (timeToBuild blueprint robots materials Ore) + 1
-                            let usedMaterials = subtract materials blueprint.oreCost
-                            match waitTurns <
+        count <- count + 1L
 
-                            recurse {robots with ore=(robots.ore + 1)} (produce robots (subtract materials blueprint.oreCost)) (turns - waitTurns)
-                        | Clay -> recurse {robots with clay=(robots.clay + 1)} (produce robots (subtract materials blueprint.clayCost)) nextTurn
-                        | Obsidian -> recurse {robots with obsidian=(robots.obsidian + 1)} (produce robots (subtract materials blueprint.obsidianCost)) nextTurn
-                        | Geode -> recurse {robots with geode=(robots.geode + 1)} (produce robots (subtract materials blueprint.geodeCost)) nextTurn)
-                    |> Array.max
-            cache.Add(entry, result)
-            result
+        let options = getOptions blueprint robots materials turns
+        // if logging then
+        //     printfn "options: %A" options
+        options
+        |> Array.map (fun robot ->
+            // if logging then
+            //     printfn "option: %A" robot
+            let waitTurns = (timeToBuild blueprint robots materials robot) + 1
+            // if logging then
+            //     printfn "time to build: %A" waitTurns
 
-    recurse {defaultMinerals with ore=1} defaultMinerals turns
+            match waitTurns < turns with
+            | false ->
+                let final = produce robots materials turns
+                final.geode
+            | true ->
+                let cost = getCost blueprint robot
+
+                let beforeBuild = produce robots materials waitTurns
+                let afterBuild = subtract beforeBuild cost
+
+                let newRobots = buildRobot robots robot
+
+                recurse newRobots afterBuild (turns - waitTurns) log)
+        |> Array.max
+
+    let result = recurse {defaultMinerals with ore=1} defaultMinerals turns false
+    // printfn $"finished in {count} iterations"
+    result
 
 let partOne (blueprints: array<Blueprint>) =
     blueprints
     |> Array.map (fun blueprint ->
         let result = run 24 blueprint
-        printfn $"{blueprint.id}: {result}, score: {result * blueprint.id}"
+        // printfn $"{blueprint.id}: {result}, score: {result * blueprint.id}"
         result * blueprint.id)
     |> Array.sum
 
@@ -145,8 +194,9 @@ let partTwo (blueprints: array<Blueprint>) =
     |> Array.reduce ( * )
 
 let testInput = parse "day19/test_input.txt"
-// assert (partOne testInput = 33)
-partTwo testInput |> printfn "%i"
+assert (partOne testInput = 33)
+assert (partTwo testInput = (56 * 62))
 
 let input = parse "day19/input.txt"
-// partOne input |> printfn "%A"
+partOne input |> printfn "%i"
+input |> Array.take 3 |> partTwo |> printfn "%i"
